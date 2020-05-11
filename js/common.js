b4deaf2479c2ff32892002ccc01c7f98dd775ec6
purelyweb <49037603+purelyweb@users.noreply.github.com>
2020-05-11 18:03:01 +0300

    move zoom slider js code from .html to .js file

--- js/common.js
+++ js/common.js
@@ -841,3 +841,17 @@ $('.cateogry-view__button_filter').on('click', function(){
 });
 
 // Сказать Олегу пусть сделает открытие подписки один раз за сессию
+
+$(function () {
+    $(".slider").slick({
+        autoplay: false,
+        dots: true,
+        arrows: false,
+        customPaging : function(slider, i) {
+            var thumb = $(slider.$slides[i]).data('thumb');
+            return '<a><img src="'+thumb+'"></a>';
+        },
+    });
+
+    $('.ex1').zoom();
+});
